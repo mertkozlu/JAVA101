bbac1c8cbec3afb57b805e21d843dacd761531bc
mertkozlu <kozlumert@outlook.com>
2023-04-07 18:18:22 +0300

    java101

---
+++ src/main/JAVA101/Metot.java
@@ -0,0 +1,25 @@
+public class Metot {
+
+    static void helloWorl() {
+        System.out.println("Hello World !");
+    }
+    static int power(int base, int exp) {
+        int resault = 1;
+        for (int i = 1; i <= exp; i++) {
+            resault *= base;
+        }
+        return resault;
+    }
+    public static void main(String[] args) {
+        System.out.println(power(2,3));
+        System.out.println(power(4,2));
+        helloWorl();
+
+        /*
+        int n1 = 2 , n2 = 3;
+        int case1 = power(n1,n2);
+        System.out.println(case1);
+         */
+
+    }
+}
