ba332c0e0db53c6d754de3309e3e45eb633385cd
mertkozlu <kozlumert@outlook.com>
2023-04-13 10:55:50 +0300

    Java101

---
+++ src/main/JAVA101/OOP/MaasHesaplayici/Main.java
@@ -0,0 +1,9 @@
+package OOP.MaasHesaplayici;
+
+public class Main {
+    public static void main(String [] args) {
+        Employee m1 = new Employee("Mert",2000,50,2020);
+        m1.printEmploye();
+    }
+}
+
